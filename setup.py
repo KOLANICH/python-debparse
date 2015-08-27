d08c39a61091f41f16e702a7a589385e9db405fc
Kirill Sibirev <sibirev@yandex-team.ru>
2015-08-27 11:15:12 +0300

    Improve test_requirements installation

--- setup.py
+++ setup.py
@@ -54,5 +54,8 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    extras_require={
+        'tests': test_requirements,
+    }
 )
